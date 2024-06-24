43b4883e480005f2deababce010bda33f5e891bf
kruna-ctrl <krunaljethva90@gmail.com>
2024-06-24 09:52:42 +0530

    Added Platform Service

---
+++ PlatformService/PlatformService/Data/IPlatformRepo.cs
@@ -0,0 +1,11 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public interface IPlatformRepo
+{
+    bool SaveChanges();
+    IEnumerable<Platform> GetAll();
+    Platform? Get(int id);
+    void Create(Platform platform);
+}
--- PlatformService/PlatformService/Program.cs
+++ PlatformService/PlatformService/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -14,6 +21,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapControllers();
 app.UseHttpsRedirection();
+app.PrepPopulation();
 
 app.Run();
