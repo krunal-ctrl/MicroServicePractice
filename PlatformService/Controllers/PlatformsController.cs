db7508ec263dc25cb48f1354a8e3c7824853b83e
kruna-ctrl <krunaljethva90@gmail.com>
2024-06-25 22:12:55 +0530

    Restructured project files

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PlatformsController(IPlatformRepo platformRepo, IMapper mapper): ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+    {
+        var enumerable = platformRepo.GetAll();
+        return Ok(mapper.Map<IEnumerable<PlatformReadDto>>(enumerable));
+    }
+
+    [HttpGet("{id:int}", Name = "GetPlatformById")]
+    public ActionResult<PlatformReadDto> GetPlatformById(int id)
+    {
+        var platform = platformRepo.Get(id);
+        if (platform == null)
+        {
+            return NotFound();
+        }
+        return Ok(mapper.Map<PlatformReadDto>(platform)); 
+    }
+
+    [HttpPost]
+    public ActionResult<PlatformReadDto> Create(PlatformCreateDto platform)
+    {
+        var platformModel = mapper.Map<Platform>(platform);
+        platformRepo.Create(platformModel);
+
+        var platformReadDto = mapper.Map<PlatformReadDto>(platformModel);
+        return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
+    }
+}
---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,31 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public class PlatformRepo(AppDbContext dbContext): IPlatformRepo
+{
+    public bool SaveChanges()
+    {
+        return dbContext.SaveChanges() >= 0;
+    }
+
+    public IEnumerable<Platform> GetAll()
+    {
+        return dbContext.Platforms.AsEnumerable();
+    }
+
+    public Platform? Get(int id)
+    {
+        return dbContext.Platforms.FirstOrDefault(x => x.Id == id);
+    }
+
+    public void Create(Platform platform)
+    {
+        if (platform is null)
+        {
+            throw new ArgumentNullException();
+        }
+        dbContext.Platforms.Add(platform);
+        SaveChanges();
+    }
+}
